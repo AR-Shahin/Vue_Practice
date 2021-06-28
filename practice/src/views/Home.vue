377fefe8a82cc6f3b9d17e844c69f5010491f3dd
AR Shahin <mdshahinmije96@gmail.com>
2021-06-29 00:05:37 +0600

    -D

--- practice/src/views/Home.vue
+++ practice/src/views/Home.vue
@@ -3,26 +3,47 @@
    <h1 class="text-center my-3">Home Page</h1>
   <input ref="custom" />
   <div ref="divRef"></div>
+  <TestComponent :title="test" @custom-event="myCustomEvent($event)"/>
+  <button @click="change" class="btn btn-sm btn-danger">Click</button> <br>
+  {{ test }}
   </div>
 </template>
 
 <script>
 
-
+import TestComponent from '../components/TestComponent.vue'
 export default {
   name: 'Home',
+  data(){
+      return {
+          test : 'Hello '
+      }
+  },
   components: {
-    
+    TestComponent
   },
  methods: {
     customRef() {
       this.$refs.custom.value = 'Shahin'
       //console.log(this.$refs);
       this.$refs.divRef.innerHTML = 'ddd'
+    },
+    myCustomEvent(e){
+        console.log(e);
+        this.test = e.title
+    },
+    change(){
+        this.test = 'hanid'
     }
   },
   mounted() {
     this.customRef()
+  },
+  updated(){
+    console.log('update')
+  },
+  unmounted(){
+    console.log('destroy')
   }
 }
 </script>
